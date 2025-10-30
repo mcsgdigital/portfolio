d890f8e9d84377b40b9edf0c2a236951db882aa5
MarkC147 <mark147chantel@gmail.com>
2025-10-30 17:21:06 +0000

    netlify-fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   output: 'export', // Ensures the app is exported as a static site
   distDir: 'out', // Optional: Specify the output folder (default is "out")
+  reactStrictMode: true,
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
